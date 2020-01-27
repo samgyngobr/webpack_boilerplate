76ca7b9d69e89f8fab9ac65ef04c6deaeadc2e19
samgyngobr <samuel0360@gmail.com>
2020-01-27 14:19:25 -0300

    ...

--- webpack/config/webpack.dev.js
+++ webpack/config/webpack.dev.js
@@ -24,8 +24,20 @@ module.exports = {
           }
         },
         {
-          test: /\.(png|jpg)$/,
-          loader: 'url-loader'
+          // Now we apply rule for images
+          test: /\.(png|jpe?g|gif|svg)$/,
+          use: [
+            {
+              // Using file-loader for these files
+              loader: "file-loader",
+
+              // In options we can set different things like format
+              // and directory to save
+              options: {
+                outputPath: 'images'
+              }
+            }
+          ]
         },
         {
           test: /\.css$/,
